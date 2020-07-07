88792382f609fd66d30754702c2af12b86f86e11
Crisme24 <crisme-24@hotmail.com>
2020-07-07 19:57:32 +0100

    Adding knockout observable variable to track task changes

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -2,5 +2,22 @@ define(['uiComponent'], function (Component) {
     'use strict';
 
     //console.log('Component');
-    return Component.extend({});
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+            ]
+        },
+
+        initObservable: function () {
+            this._super().observe(['tasks']);
+
+            this.tasks().push({label: 'Task 5'});
+
+            return this;
+        },
+    });
 });
