dad1e2b166a0666e6d4d913e1d1a367908979131
Crisme24 <crisme-24@hotmail.com>
2020-07-15 21:15:38 +0100

    Adding logic to switch between task statuses with knockout data-bind attribute

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,14 +1,17 @@
-define(['uiComponent'], function (Component) {
+define([
+    'uiComponent',
+    'jquery'
+], function (Component, $) {
     'use strict';
 
     //console.log('Component');
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task 1"},
-                {label: "Task 2"},
-                {label: "Task 3"},
-                {label: "Task 4"},
+                {id: 1, label: "Task 1", status: false},
+                {id: 2, label: "Task 2", status: false},
+                {id: 3, label: "Task 3", status: false},
+                {id: 4, label: "Task 4", status: true},
             ]
         },
 
@@ -19,5 +22,19 @@ define(['uiComponent'], function (Component) {
 
             return this;
         },
+
+        switchStatus: function (data, event) {
+            const taskId = $(event.target).data('id');
+
+            var items = this.tasks().map(function (task) {
+                if (task.id === taskId) {
+                    task.status = !task.status;
+                }
+
+                return task;
+            });
+
+            this.tasks(items);
+        } 
     });
 });
