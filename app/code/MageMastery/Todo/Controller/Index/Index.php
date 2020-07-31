754699fead5466696602204632afcb467aa55a7d
Crisme24 <crisme-24@hotmail.com>
2020-07-31 19:51:21 +0100

    Creating Task Repository service

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -9,6 +9,7 @@ use Magento\Framework\Controller\ResultFactory;
 use MageMastery\Todo\Model\Task;
 use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 use MageMastery\Todo\Model\TaskFactory;
+use MageMastery\Todo\Service\TaskRepository;
 
 class Index extends Action
 {
@@ -16,10 +17,21 @@ class Index extends Action
 
     private $taskFactory;
 
-    public function __construct(Context $context, TaskFactory $taskFactory, TaskResource $taskResource)
+    /**
+     * @var TaskRepository
+     */
+    private $taskRepository;
+
+    public function __construct(
+        Context $context, 
+        TaskFactory $taskFactory, 
+        TaskResource $taskResource,
+        TaskRepository $taskRepository
+        )
     {
         $this->taskFactory = $taskFactory;
         $this->taskResource = $taskResource;
+        $this->taskRepository = $taskRepository;
 
         parent::__construct($context);
 
@@ -27,15 +39,18 @@ class Index extends Action
 
     public function execute()
     {
-        $task = $this->taskFactory->create();
+        $task = $this->taskRepository->get(1);
+        var_dump($task->getData());
 
-        $task->setData([
-            'label' => 'New Task 22',
-            'status' => 'open',
-            'customer_id' => 1
-        ]);
+        // $task = $this->taskFactory->create();
 
-        $this->taskResource->save($task);
+        // $task->setData([
+        //     'label' => 'New Task 22',
+        //     'status' => 'open',
+        //     'customer_id' => 1
+        // ]);
+
+        // $this->taskResource->save($task);
 
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
---
+++ app/code/MageMastery/Todo/Service/TaskRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MageMastery\Todo\Service;
+
+use MageMastery\Todo\Api\TaskRepositoryInterface;
+use MageMastery\Todo\Model\ResourceModel\Task;
+use MageMastery\Todo\Model\TaskFactory;
+
+class TaskRepository implements TaskRepositoryInterface
+{
+    /**
+     * @var Task
+     */
+    private $resource;
+
+    /**
+     * @var TaskFactory
+     */
+    private $taskFactory;
+
+    public function __construct(
+        Task $resource, 
+        TaskFactory $taskFactory
+        )
+    {
+        $this->resource = $resource;
+        $this->taskFactory = $taskFactory;
+    }
+
+    public function getList()
+    {
+        //TODO: Implement getList() method.
+    }
+
+    public function get(int $taskId)
+    {
+        //TODO: Implement get() method.
+        $object = $this->taskFactory->create();
+        $this->resource->load($object, $taskId);
+        return $object;
+    }
+}
