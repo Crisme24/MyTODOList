55df92e170c9eeb89917808d5571d8a398c76061
Crisme24 <crisme-24@hotmail.com>
2020-07-31 15:26:06 +0100

    Creating TODO Service Api interfaces

---
+++ app/code/MageMastery/Todo/Api/TaskManagementInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MageMastery\Todo\Api;
+
+/**
+ * @api
+ */
+interface TaskManagementInterface
+{
+    public function save();
+    public function delete();
+}
---
+++ app/code/MageMastery/Todo/Api/TaskRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MageMastery\Todo\Api;
+
+/**
+ * @api
+ */
+interface TaskRepositoryInterface
+{
+    public function getList();
+    public function get(int $taskId);
+}
